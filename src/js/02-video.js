e7d715d2025edfeb40a60222d78980522e5a3871
KulibabaYuliia <kulibaba120@gmail.com>
2023-08-26 01:03:02 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
