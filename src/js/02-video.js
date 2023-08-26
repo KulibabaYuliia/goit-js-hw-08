42e86ac1fa029bd1fca587781288e7ceafcda555
KulibabaYuliia <kulibaba120@gmail.com>
2023-08-26 18:39:43 +0300

    third task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,6 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
 player.on('timeupdate', throttle(currentTime, 1000));
 
 function currentTime(data) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = formEl.querySelector("input[name = 'email']");
+const messageEl = formEl.querySelector("textarea[name = 'message']");
+
+const feedback = {
+  email: '',
+  message: '',
+};
+
+formEl.addEventListener('input', throttle(onFormInputHandler, 500));
+
+function onFormInputHandler(event) {
+  if (event.target.name === 'email') {
+    feedback.email = event.target.value.trim();
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
+  }
+  if (event.target.name === 'message') {
+    feedback.message = event.target.value.trim();
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
+  }
+}
+
+function setFormValues() {
+  if (!localStorage.getItem('feedback-form-state')) {
+    return;
+  }
+  emailEl.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+  messageEl.value = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).message;
+}
+
+setFormValues();
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault;
+  localStorage.clear();
+  emailEl.value = '';
+  messageEl.value = '';
+});
